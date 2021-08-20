f376b583eb73a30e37f56ac0d18d53d114edd94e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-08-20 15:15:54 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -825,11 +825,13 @@ class PaymentService
              $paymentData['paid_amount'] = 0;
              }
              $paymentData['booking_text'] = $transactionComments;  
-             $this->paymentHelper->updatePayments($tid, $responseData['tid_status'], $order->id);
+             
              if($responseData['tid_status'] == '103') {
+		$this->paymentHelper->updatePayments($transactionComments, $responseData['tid_status'], $order->id);
                 $this->paymentHelper->cancelPlentyOrder($order->id);
-            }
+            } else {
              $this->paymentHelper->createPlentyPayment($paymentData);
+	     }
          } else {
                $error = $this->paymentHelper->getNovalnetStatusText($responseData);
                $this->getLogger(__METHOD__)->error('Novalnet::doCaptureVoid', $error);
